b701beeae8243606018216445313ab1fb58e775c
Michael Golden <mgolden@mgolden.hsd1.pa.comcast.net>
2021-03-13 16:14:29 -0500

    603_add_Google_map_support

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -123153,7 +123153,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51949" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56440" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
