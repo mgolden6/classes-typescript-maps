bbbe36800d8010928699b6856f35284859ebab2c
Michael Golden <mgolden@mgolden.hsd1.pa.comcast.net>
2021-03-13 20:40:02 -0500

    610_refactor_markers

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
+import { CustomMap } from './CustomMap';
 import { User } from './User';
 import { Company } from './Company';
-import { CustomMap } from './CustomMap';
 
 const customMap = new CustomMap('map');
 const user = new User();
 const company = new Company();
 
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+customMap.addMarker(user);
+customMap.addMarker(company);
